f21d37c9b036ac93e8c4192b9930fda5ba622373
nick@punchcyber.com <nick@punchcyber.com>
2015-02-18 18:16:27 -0500

    fixed tor.sh, updated install.sh

--- config/testing/tor.sh
+++ config/testing/tor.sh
@@ -12,19 +12,16 @@ LOG_PATH=$MAIN_PATH/logs
 UB_PATH=/usr/bin
 INST="apt-get install -y"
 
-exec > >(tee -a $LOG_PATH/install.log)
-exec 2> >(tee -a $LOG_PATH/install-errors.log)
-
 # Install Tor
 install_tor() {
     if [ -f /usr/bin/tor ]; then
         echo Tor is already installed
     else
-        echo Installing Tor. . .
-        chmod 666 /etc/apt/sources.list
-        echo deb http://deb.torproject.org/torproject.org trusty main >> /etc/apt/sources.list
-        chmod 644 /etc/apt/sources.list
-        echo added deb http://deb.torproject.org/torproject.org trusty main to source.list
+        cat /etc/apt/sources.list | grep -q "deb.torproject.org"
+        if [ $? -ne 0 ]; then
+            echo "deb http://deb.torproject.org/torproject.org trusty main" | sudo tee -a /etc/apt/sources.list
+            echo added deb http://deb.torproject.org/torproject.org trusty main to source.list
+        fi
 
         apt-key list | grep -q "deb.torproject.org archive signing key";
         if [ $? -ne 0 ]; then
@@ -38,15 +35,20 @@ install_tor() {
         fi
 
         apt-key list | grep -q "deb.torproject.org archive signing key";
-        if [ $? == 0 ]; then
+        if [ $? -ne 0 ]; then
             gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
-            apt-get updatesu
-            apt-get install deb.torproject.org-keyring
-            apt-get install -y tor
+        fi
 
-            echo ——————————————————————————————————————————————— >> $LOG_PATH/install.log
-            echo   >> $LOG_PATH/install.log
-            echo Tor installation complete.
+        apt-key list | grep -q "deb.torproject.org archive signing key";
+        if [ $? == 0 ]; then
+            apt-get updates
+            $INST deb.torproject.org-keyring
+            echo installing tor
+            $INST tor
+        fi
+
+        if [ -f /usr/bin/tor ]; then
+            echo Tor is installed
         else
             echo Tor is unable to install, see: https://www.torproject.org/docs/debian
         fi
