0a5add802f2d3f13c7e3eadcd6dd83110d285832
nick@punchcyber.com <nick@punchcyber.com>
2015-02-21 16:57:29 -0500

    added wine install script

---
+++ config/testing/wine.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#  wine.sh
+#  
+#
+#  Created by Remis on 2/20/15.
+#
+
+
+#install wine
+install_wine(){
+    if [ -f /usr/bin/wine ]; then
+        continue
+    else
+        dpkg --add-architecture i386
+        apt-get update
+        $INST wine1.6
+    fi
+}
+
+install_wine
